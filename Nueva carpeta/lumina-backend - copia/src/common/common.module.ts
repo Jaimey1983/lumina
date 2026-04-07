dcf57bdb0f580563b9e96a8d6d5962997ed7a077
Jaimey1983quiero <jeg19837@gmail.com>
2026-04-06 20:49:45 -0500

    chore: remove backup copies folder

--- Nueva carpeta/lumina-backend - copia/src/common/common.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { CourseAuthorizationService } from './course-authorization.service';
-
-@Global()
-@Module({
-  providers: [CourseAuthorizationService],
-  exports: [CourseAuthorizationService],
-})
-export class CommonModule {}
--- Nueva carpeta/lumina-frontend - copia/src/app/(auth)/login/page.tsx
+++
@@ -1,8 +0,0 @@
-import type { Metadata } from 'next';
-import { LoginClient } from './login-client';
-
-export const metadata: Metadata = { title: 'Iniciar sesión' };
-
-export default function LoginPage() {
-  return <LoginClient />;
-}
