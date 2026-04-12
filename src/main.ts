73733bb51d847c73d7bd63e191e9cb9f913dd078
Jaimey1983quiero <jeg19837@gmail.com>
2026-04-12 13:25:48 -0500

    fix: aumentar límite body parser a 50mb para imágenes y videos en base64

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { json, urlencoded } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
+
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
 
   app.useGlobalPipes(
     new ValidationPipe({
